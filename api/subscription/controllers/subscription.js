c49f3196ca726871ffc94af0322addc085fa4514
unknown <shasongurung@gmail.com>
2021-04-29 14:40:56 +0100

    created custom route and controller in subscription section to send subscription message to subscriber

--- api/subscription/controllers/subscription.js
+++ api/subscription/controllers/subscription.js
@@ -5,4 +5,30 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async confirmation(ctx) {
+        let msgDestination='shasongurung@gmail.com';
+        let {subscriberEmail} = ctx.request.body;
+        let msgDataSent={
+            from:msgDestination,
+            replyTo:msgDestination,
+            to:subscriberEmail,
+        }
+        try {
+            await strapi.plugins['email-designer'].services.email.sendTemplatedEmail(
+                msgDataSent,
+                {templateId:3},
+                {}
+            );
+        } catch (e) {
+            if (e.statusCode === 400) {
+            return ctx.badRequest(e.message);
+            } else {
+            throw new Error(`Couldn't send email: ${e.message}.`);
+            }
+        }
+    
+        // Send 200 `ok`
+        ctx.send({});
+      },
+};
